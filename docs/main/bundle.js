ee90bff0df8a8460dd8a0550d32339b3c48b8eb4
Rahul Kumar Singh <37482171+rahulkumarsingh73690@users.noreply.github.com>
2019-08-30 16:47:45 +0530

    Update bundle.js

--- docs/main/bundle.js
+++ docs/main/bundle.js
@@ -26,23 +26,7 @@ arguments[4][1][0].apply(exports,arguments)
 },{"dup":1}],10:[function(require,module,exports){
 arguments[4][1][0].apply(exports,arguments)
 },{"dup":1}],11:[function(require,module,exports){
-module.exports = `Type in here to get a live word count.
-
-It's smart, so symbol separated words get counted properly
-
-people+trying*to%cheat-by|using_symbols=to*split#words is 7 words
-
-
-But ASCII art or tables are fine
-
-| d4 | Result |
----------------
-| 1  | Sock   |
-| 2  | Shoe   |
-| 3  | Pants  |
-| 4  | Hat    |
-
-is only 10 words`;
+module.exports = `Type here to get a live word count.`;
 },{}],12:[function(require,module,exports){
 const React = require('react');
 
